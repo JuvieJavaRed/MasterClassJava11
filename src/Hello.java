60d2e94eb6100abdcc52d5eb896df61ac6d07fd7
Mthokozisi Nyoni <mthokozisi.nyoni@entelect.co.za>
2022-01-13 10:52:52 +0200

    Varibles and Expressions

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hie Mtho");
+
+        //int a whole number without a decimal
+        int myFirstNumber = 35;
+
+        //int my second number
+        int mySecondNumber = 12;
+
+        //int my third number
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        //printing out my first number
+        System.out.println("This is my total : "+myTotal);
+    }
+}
